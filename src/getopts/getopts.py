c11b60b9c1aa09f1faf27f1170840bd40011ff2b
Mark Kim <markuskimius@gmail.com>
2022-07-05 08:01:08 -0400

    v1.0.3

--- src/getopts/getopts.py
+++ src/getopts/getopts.py
@@ -3,10 +3,13 @@
 https://github.com/markuskimius/getopt-py
 """
 
-import sys, re
-
-__copyright__ = "Copyright 2019-2020 Mark Kim"
+__copyright__ = "Copyright 2019-2022 Mark Kim"
 __license__ = "Apache 2.0"
+__version__ = "1.0.3"
+__author__ = "Mark Kim"
+__all__ = ["getopts"]
+
+import sys, re
 
 class getopts(object):
     NONOPTION = "-"
